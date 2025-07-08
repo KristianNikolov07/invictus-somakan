b6102dbb046d45048898ffb1f711b85599d73ef4
KristianNikolov07 <knpronoob@gmail.com>
2025-07-08 12:06:37 +0300

    Made claws and added a Weapon class

--- Scripts/Weapons/sword.gd
+++ Scripts/Weapons/sword.gd
@@ -1,8 +1,4 @@
-extends Node
-@export var attack_damage = 10
-@export var crit_chance = 0.1
-@export var crit_mult = 2
-@export var knockback_amount = 1
+extends Weapon
 var dir = 1
 var is_down = false
 
