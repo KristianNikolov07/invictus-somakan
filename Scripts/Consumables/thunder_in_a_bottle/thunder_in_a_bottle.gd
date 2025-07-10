8664a3081e02591f63ea4301563156160fa3ffba
KristianNikolov07 <knpronoob@gmail.com>
2025-07-10 12:12:04 +0300

    Added the thunder in a bottle

--- Scripts/Consumables/thunder_in_a_bottle/thunder_in_a_bottle.gd
+++ Scripts/Consumables/thunder_in_a_bottle/thunder_in_a_bottle.gd
@@ -3,9 +3,10 @@ var bottle = preload("res://Scenes/Consumables/thunder_in_a_bottle/bottle.tscn")
 
 func use(player_path: String):
 	var player = get_node(player_path)
-	var direction = player
+	var target = get_global_mouse_position()
+	target.y = player.global_position.y
 	var bottle: Projectile = bottle.instantiate()
 	bottle.global_position = global_position
-	bottle.rotation = global_position.direction_to(target.global_position).angle()
-	bottle.shooter_vel = velocity
+	bottle.rotation = global_position.direction_to(target).angle()
+	bottle.shooter_vel = player.velocity
 	get_tree().current_scene.add_child(bottle)
