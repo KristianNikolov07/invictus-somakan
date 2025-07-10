87fe48024a0933690056f7f41c0df9f98efafd54
KristianNikolov07 <knpronoob@gmail.com>
2025-07-10 14:44:32 +0300

    Added the Karen Head

--- Scripts/Globals/Utils.gd
+++ Scripts/Globals/Utils.gd
@@ -1,14 +1,15 @@
 extends Node
 
 func summon_damage_number(body: CharacterBody2D, damage: int, color: Color, scale: float, duration: float):
-	var damage_number = DamageNumber.new()
-	body.get_parent().add_child(damage_number)
-	damage_number.text = str(damage)
-	damage_number.global_position = body.global_position + Vector2(0, -75)
-	damage_number.set_duration(duration)
-	damage_number.add_theme_color_override("font_color", color)
-	damage_number.scale = Vector2(scale, scale)
-	damage_number.start_playing()
+	if damage > 0:
+		var damage_number = DamageNumber.new()
+		body.get_parent().add_child(damage_number)
+		damage_number.text = str(damage)
+		damage_number.global_position = body.global_position + Vector2(0, -75)
+		damage_number.set_duration(duration)
+		damage_number.add_theme_color_override("font_color", color)
+		damage_number.scale = Vector2(scale, scale)
+		damage_number.start_playing()
 
 func calculate_crit(crit_chance: float):
 	return true if randf_range(0, 1) < crit_chance else false
