1eca897f1aa476af91235723d697206ed8475523
toniabv69 <105800089+toniabv69@users.noreply.github.com>
2025-07-11 16:03:52 +0300

    idk what's going on

--- Scripts/Classes/Aspect.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-class_name AspectItem
-
-@export var status_effect: PackedScene
-@export var aspect_name: String
-
-func apply_effect(body):
-	pass
--- Scripts/Classes/AspectItem.gd
+++ Scripts/Classes/AspectItem.gd
@@ -5,7 +5,7 @@ class_name AspectItem
 @export var aspect_name: String
 @export var crit_chance_mult: float = 1
 @export var crit_damage_mult: float = 1
-@export var summoned_on_impact: PackedScene
+@export var explosion: bool = false
 
 func apply_crit_stats(hitbox: Hitbox):
 	hitbox.crit_chance *= crit_chance_mult
@@ -18,7 +18,5 @@ func unapply_crit_stats(hitbox: Hitbox):
 func apply_effect(body: CharacterBody2D):
 	if status_effect != null and body.has_method("apply_status_effect"):
 		body.apply_status_effect(status_effect)
-	if summoned_on_impact != null:
-		var summoned = summoned_on_impact.instantiate()
-		summoned.position = body.position
-		body.get_parent().add_child(summoned)
+	if explosion:
+		Utils.summon_explosion(body.position, 0.8, 5, 1, 1.2, false, true)
