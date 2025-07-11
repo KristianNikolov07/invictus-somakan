17a46ea7b439b9f2ecee033d184be0e833753f94
cursed144 <107793642+cursed144@users.noreply.github.com>
2025-07-11 12:25:32 +0300

    Maeksman done + recoded the weapon switching system

--- Scripts/Projectiles/bullet.gd
+++ Scripts/Projectiles/bullet.gd
@@ -1,8 +1,9 @@
 extends Projectile
 
 
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Enemies") and can_hit_enemies:
 		body.damage_amount(damage, knockback)
+		queue_free()
+	elif not body.is_in_group("Players"):
 		queue_free()
