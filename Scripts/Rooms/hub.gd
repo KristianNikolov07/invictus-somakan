a424dd94bb3de8fca7a8dfb3081b136c6f14fb1d
KNProNooob <138398165+KristianNikolov07@users.noreply.github.com>
2025-07-13 21:10:05 +0300

    Room number now gets reset if you go back to the hub

--- Scripts/Rooms/hub.gd
+++ Scripts/Rooms/hub.gd
@@ -2,3 +2,15 @@ extends Node2D
 
 func _ready() -> void:
 	PlayerStats.save_stats(PlayerStats.current_save_file)
+	PlayerStats.hp = PlayerStats.max_hp
+	
+	PlayerStats.remove_consumable(0, 1000)
+	PlayerStats.remove_consumable(1, 1000)
+	
+	PlayerStats.set_aspect(1, 0, null)
+	PlayerStats.set_aspect(1, 1, null)
+	
+	PlayerStats.set_aspect(2, 0, null)
+	PlayerStats.set_aspect(2, 1, null)
+	
+	RoomGen.current_room = 0
