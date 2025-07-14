d6129855ab14af19f0b82e50c1a97f90fbaa219a
cursed144 <107793642+cursed144@users.noreply.github.com>
2025-07-14 10:28:01 +0300

    sniper fixed

---
+++ Scripts/Weapons/chushkopek_sniper.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+const pepper := preload("res://Scenes/Projectiles/pepper.tscn")
+@onready var player = get_node("../../")
+
+
+func hit(_direction):
+	var cooking_phase
+	if $Heat.value < 290:
+		cooking_phase = 1
+		$Heat.value -= 50
+	elif $Heat.value >= 290 and $Heat.value < 540:
+		cooking_phase = 2
+		$Heat.value -= 75
+	elif $Heat.value >= 540 and $Heat.value < 790:
+		cooking_phase = 3
+		$Heat.value -= 150
+	else:
+		$Heat.value -= 1000
+		cooking_phase = 4
+	
+	var new_pepper: Projectile = pepper.instantiate()
+	new_pepper.cooking_phase = cooking_phase
+	new_pepper.global_position = player.global_position
+	new_pepper.rotation = player.global_position.direction_to(get_global_mouse_position()).angle()
+	new_pepper.shooter_vel = player.velocity
+	get_tree().current_scene.add_child(new_pepper)
+
+
+func _on_refresh_bar_timeout() -> void:
+	$Heat.value += 1
