a69cac2794b1dc3f02a1f7d0651d75377d98ea1d
KNProNooob <knpronoob@gmail.com>
2025-07-23 14:10:44 +0300

    Added a tutorial level

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -13,6 +13,7 @@ var speed_mult = 1
 var direction = 1
 var damage_number_scale: float = 1.5
 var damage_number_duration: float = 1.5
+@export var can_take_damage = true
 
 var selected_weapon
 
@@ -58,11 +59,11 @@ func _input(event: InputEvent) -> void:
 	elif event.is_action_pressed("Weapon2"):
 		switch_weapon(PlayerStats.weapon2)
 
-	#elif event.is_action_released("PreviousWeapon") or event.is_action_released("NextWeapon"):
-	#	if selected_weapon.item_name == PlayerStats.weapon1.item_name:
-	#		selected_weapon = PlayerStats.weapon2
-	#	else:
-	#		selected_weapon = PlayerStats.weapon1
+	elif event.is_action_released("PreviousWeapon") or event.is_action_released("NextWeapon"):
+		if selected_weapon.item_name == PlayerStats.weapon1.item_name:
+			switch_weapon(PlayerStats.weapon2)
+		else:
+			switch_weapon(PlayerStats.weapon1)
 	elif event.is_action_pressed("UseConsumable1") and $PlayerSprite.animation != "dash":
 		use_consumable(1)
 	elif event.is_action_pressed("UseConsumable2") and $PlayerSprite.animation != "dash":
@@ -214,18 +215,19 @@ func open_shop_menu():
 
 func damage_amount(amount: int, knockback) -> void:
 	Utils.summon_damage_number(self, amount, Color.RED, damage_number_scale, damage_number_duration)
-	set_collision_layer_value(1, false)
-	$Invincibility.start()
-	velocity.x = 1600 * knockback
-	velocity.y = -500 * abs(knockback)
-	PlayerStats.hp -= amount
-	$Parry.stop()
-	if PlayerStats.hp <= 0:
-		var tree = get_tree()
-		$PlayerSprite.play("death")
-		await $PlayerSprite.animation_finished
-		if is_instance_valid(tree):
-			tree.change_scene_to_file("res://Scenes/UI/game_over.tscn")
+	if can_take_damage:
+		set_collision_layer_value(1, false)
+		$Invincibility.start()
+		velocity.x = 1600 * knockback
+		velocity.y = -500 * abs(knockback)
+		PlayerStats.hp -= amount
+		$Parry.stop()
+		if PlayerStats.hp <= 0:
+			var tree = get_tree()
+			$PlayerSprite.play("death")
+			await $PlayerSprite.animation_finished
+			if is_instance_valid(tree):
+				tree.change_scene_to_file("res://Scenes/Rooms/Hub/hub.tscn")
 
 	
 func damage(hitbox: Hitbox, knockback):
@@ -237,13 +239,14 @@ func damage(hitbox: Hitbox, knockback):
 	velocity.y = -500 * abs(knockback)
 	var damage_num = hitbox.get_damage() * (hitbox.get_crit_mult() if is_crit else 1)
 	Utils.summon_damage_number(self, damage_num, Color.RED, damage_number_scale, damage_number_duration)
-	PlayerStats.hp -= damage_num
-	if PlayerStats.hp <= 0:
-		var tree = get_tree()
-		$PlayerSprite.play("death")
-		await $PlayerSprite.animation_finished
-		if is_instance_valid(tree):
-			tree.change_scene_to_file("res://Scenes/UI/game_over.tscn")
+	if can_take_damage:
+		PlayerStats.hp -= damage_num
+		if PlayerStats.hp <= 0:
+			var tree = get_tree()
+			$PlayerSprite.play("death")
+			await $PlayerSprite.animation_finished
+			if is_instance_valid(tree):
+				tree.change_scene_to_file("res://Scenes/Rooms/Hub/hub.tscn")
 
 
 func unlock_recipe(recipe: Recipe):
