21c439bfbef9c482707ae8fff1ea7b3fcefba396
KNProNooob <knpronoob@gmail.com>
2025-07-24 13:23:50 +0300

    Equipped Consumable amounts now showup correctly

--- Scripts/UI/Inventory/consumable_slot.gd
+++ Scripts/UI/Inventory/consumable_slot.gd
@@ -5,8 +5,12 @@ extends Control
 func set_item(_item: Item) -> void:
 	if _item != null:
 		item = _item.duplicate()
-		item.amount = 1
 		$ConsumableIcon.texture = item.icon
+		item.amount = _item.amount
+		if item.amount == 1:
+			$Amount.text = ""
+		else:
+			$Amount.text = str(item.amount)
 	else:
 		clear_item()
 
